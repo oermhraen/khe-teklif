5f177841d517a55ba0e522a482b73ea1cd6edfd6
oermhraen <eorhan93@gmail.com>
2026-08-19 10:44:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -358,7 +358,7 @@ with colA:
 
     # Varsayilan gorsel metni sablonu
     ornek_model = "KHE-P01-5-C10-CS-00 PLAKALI EŞANJÖR"
-    ornek_aciklama = "Kapasite : 100.000 kCal/h\nPrimer Devre : 90°C / 70°C - 50 kPa\nSekonder Devre : 10°C / 60°C - 50 kPa\nPlaka ve Conta Malzemesi : 316 Paslanmaz / 0,5 mm - EPDM\nGövde Malzemesi ve İşletme Basincci : Karbon Çelik - 10 Bar\nBağlanti Malzemesi ve Çapi : Karbon Çelik - 1\" Dişli"
+    ornek_aciklama = "Kapasite : 100.000 kCal/h\nPrimer Devre : 90°C / 70°C - 50 kPa\nSekonder Devre : 10°C / 60°C - 50 kPa\nPlaka ve Conta Malzemesi : 316 Paslanmaz / 0,5 mm - EPDM\nGövde Malzemesi ve İşletme Basıncı : Karbon Çelik - 10 Bar\nBağlantı Malzemesi ve Çapi : Karbon Çelik - 1\" Dişli"
 
     model_input = st.text_input("Ürün Kodu / Başlik", value=ornek_model)
     aciklama_input = st.text_area("İçerik / Teknik Özellikler", value=ornek_aciklama, height=180)
